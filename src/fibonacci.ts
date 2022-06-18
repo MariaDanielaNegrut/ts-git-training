1fb1fa4d14ef284698e1d548443c586a24628a9c
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:41:57 +0100

    allow an optional negative recursive approach

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,4 +1,10 @@
-export const computeFibonacciNumber = (position: number): number => {
+export const computeFibonacciNumber = (position: number, isRecursive: boolean = false): number => {
+    if (isRecursive) return recursiveFibonacci(position);
+
+    if (position <= 2) {
+        return 1;
+    }
+
     if (position === 1 || position === 2) {
         return 1;
     }
@@ -15,3 +21,14 @@ export const computeFibonacciNumber = (position: number): number => {
     }
     return largeFibonacciNumber;
 };
+
+const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
+    const currentPosition = position ?? initialPosition;
+    if (initialPosition === 0) return 0;
+    if (currentPosition === 0) return left;
+    if (initialPosition > 0) {
+        return recursiveFibonacci(initialPosition, right, left + right, currentPosition - 1);
+    } else {
+        return recursiveFibonacci(initialPosition, right - left, left, currentPosition + 1);
+    }
+}
